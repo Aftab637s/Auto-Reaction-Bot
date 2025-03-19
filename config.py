d54886db7a2c1c68185b00849e9d323926f97104
ll_l_sukoon_ll <168022130+Aftab637s@users.noreply.github.com>
2025-03-19 21:39:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "21316774")
-API_ID = int(environ.get("API_ID", "50f64c71c78a34a85dad33fb78fc6c33"))
+API_HASH = environ.get("API_HASH", "23907320")
+API_ID = int(environ.get("API_ID", "c3c68636fa902388c33426958b69a886)
 BOT_TOKEN = environ.get("BOT_TOKEN", "7541660634:AAFKaxGUe1qnGJuTW_Dr97V8tsMZqD0OASE")
 BOT_OWNER = int(environ.get("BOT_OWNER", "7338196784"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "@SONALI_MUSIC_FFZ_BOT)
