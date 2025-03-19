e33d358824874e933840d8ada09414e2320552fb
ll_l_sukoon_ll <168022130+Aftab637s@users.noreply.github.com>
2025-03-19 21:28:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "")
-API_ID = int(environ.get("API_ID", ""))
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-BOT_USERNAME = environ.get("BOT_USERNAME", "QuickReactRobot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
-DATABASE_URL = environ.get("DATABASE_URL", "")
+API_HASH = environ.get("API_HASH", "21316774")
+API_ID = int(environ.get("API_ID", "50f64c71c78a34a85dad33fb78fc6c33"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "7541660634:AAFKaxGUe1qnGJuTW_Dr97V8tsMZqD0OASE")
+BOT_OWNER = int(environ.get("BOT_OWNER", "7338196784"))
+BOT_USERNAME = environ.get("BOT_USERNAME", "@SONALI_MUSIC_FFZ_BOT)
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002351510969"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002545960874"))
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://surajkunaar555333:HXYCnoobDezHh6kc@cluster0.e0qih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Define default emojis list
 EMOJIS = [
-    "👍", "🤷‍♂", "❤", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", 
-    "🥶", "🤩", "🥳", "😎", "🙏", "👌", "🤣", "😇", "🥱", "🥴", "😍", "🤓", 
-    "❤‍🔥", "🌚", "😐", "💯", "🦄", "⚡", "👾", "🏆", "💔", "🤨", "🌟", "😡", 
+    "👍", "🤷‍♂", "❤", "🔥", "🥰", "👏", "💕", "🤔", "🤯", "😱", "😍", "😢", 
+    "🥶", "🤩", "🥳", "😎", "❤️‍🔥", "👌", "🤣", "😇", "🥱", "🥴", "😍", "🤓", 
+    "❤‍🔥", "🌚", "😐", "💯", "❤️", "⚡", "❤️", "🏆", "💝", "🤨", "🌟", "😘", 
     "👅", "🆒", "😘", "😈", "😴", "😭", "👻", "🌈", "👨‍💻", "👀", "🎃", "🙄", 
     "🤧", "😨", "🤝", "🤐", "🤗", "🫡", "🤭", "🥸", "🤫", "😶‍🌫", "🤪", "😏"
 ]
